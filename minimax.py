a8a9c07f97cd1ae7b6d3c4f5e6fc93b0af545de2
cledersonbc <cledersonbc@live.com>
2017-12-19 20:22:03 -0200

    Minimax done v. 2.0

--- minimax.py
+++ minimax.py
@@ -1,186 +1,132 @@
 #!/usr/bin/env python3
 from math import inf as infinity
+
 board = [
-		[  0,  0,  0 ],
-		[  0,  0,  0 ],
-		[  0,  0,  0 ],
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
 ]
 HUMAN = -1
-COMP  = +1
+COMP = +1
+
 
 def evaluate(state):
-	"""
-	Function to heuristic evaluation of state.
-	"""
-	score = 0
-	score = score + eval_line(0, 0, 0, 1, 0, 2, state) # row 0
-	score = score + eval_line(1, 0, 1, 1, 1, 2, state) # row 1
-	score = score + eval_line(2, 0, 2, 1, 2, 2, state) # row 2
-	score = score + eval_line(0, 0, 1, 0, 2, 0, state) # col 0
-	score = score + eval_line(0, 1, 1, 1, 2, 1, state) # col 1
-	score = score + eval_line(0, 2, 1, 2, 2, 2, state) # col 2
-	score = score + eval_line(0, 0, 1, 1, 2, 2, state) # diag
-	score = score + eval_line(0, 2, 1, 1, 2, 0, state) # inv. diag
-	return score
+    """
+    Function to heuristic evaluation of state.
+    :param state: the state of the current board
+    :return: +1 if the computer wins; -1 if the human wins; 0 draw
+    """
+    if game_over(state, COMP):
+        score = +1
+    elif game_over(state, HUMAN):
+        score = -1
+    else:
+        score = 0
+
+    return score
 
 
-def eval_line(r1, c1, r2, c2, r3, c3, state):
-	"""
-	This function evaluates a line and it returns:
-	* +1 or +10 or +100 for a computer player
-	* -1 or -10 or -100 for a human player
-	"""
-	score = 0
-
-	# First cell
-	# Suppose X is a computer and O is a human
-	# X ? ?; score = +1
-	# O ? ?; score = -1
-	# ? ? ?; score = 0
-	if COMP == state[r1][c1]:
-		score = +1
-	elif HUMAN == state[r1][c1]:
-		score = -1
-
-	# Second cell
-	# X X ?; score = +10 instead of +1 (more chances to win)
-	# O O ?; score = -10 instead of -1
-	# below **otherwise**
-	if COMP == state[r2][c2]:
-		if score == 1:
-			score = +10 # X X ?; may win
-		elif score == -1:
-			return 0 # O X ?; no chances to win, draw
-		else:
-			score = +1 # ? X ?; cell1 is empty
-	elif HUMAN == state[r2][c2]:
-		if score == -1:
-			score = -10 # O O ?; may lose
-		elif score == 1:
-			return 0 # X O ?; draw
-		else:
-			score = -1 # ? O ?; cell1 is empty
-
-	# Third cell
-	# X X X; score = +100 (win)
-	# O O O; score = -100 (lose)
-	# below **otherwise**
-	if COMP == state[r3][c3]:
-		if score > 0:
-			# X X X; new score = +100
-			# ? X X; new score = +10
-			score *= 100
-		elif score < 0:
-			# O O X
-			# O ? X
-			return 0 # draw
-		else:
-			# ? ? X; cell1 and cell2 are empty
-			score = +1
-	elif HUMAN == state[r3][c3]:
-		if score < 0:
-			# O O O; new score = -100
-			# ? O O; new score = -10
-			score *= 10
-		elif score > 1:
-			# X X O; draw
-			# X ? O; draw
-			return 0
-		else:
-			# ? ? O; cell1 and cell2 are empty
-			score = -1
-
-	return score
+def game_over(state, player):
+    """
+    This function tests if a specific player wins.
+    * Three rows 	[X X X] or [O O O]
+    * Three cols 	[X X X] or [O O O]
+    * Two diagonals [X X X] or [O O O]
+    :param state: the state of the current board
+    :param player: a human or a computer
+    :return: True if the player wins
+    """
+    win_state = [
+        [state[0][0], state[0][1], state[0][2]],
+        [state[1][0], state[1][1], state[1][2]],
+        [state[2][0], state[2][1], state[2][2]],
+        [state[0][0], state[1][0], state[2][0]],
+        [state[0][1], state[1][1], state[2][1]],
+        [state[0][2], state[1][2], state[2][2]],
+        [state[0][0], state[1][1], state[2][2]],
+        [state[2][0], state[1][1], state[0][2]],
+    ]
+    if [player, player, player] in win_state:
+        return True
+    else:
+        return False
 
 
-def gameover(state, player):
-	"""
-	Gameover function returns True if the player wins in the Tic-Tac-Toe
-	State is the board. To win, the player has 8 possibilities:
-	* Three rows 	[X X X] or [O O O]
-	* Three cols 	[X X X] or [O O O]
-	* Two diagonals [X X X] or [O O O]
-	"""
-	winstate = [
-		[state[0][0], state[0][1], state[0][2]],
-		[state[1][0], state[1][1], state[1][2]],
-		[state[2][0], state[2][1], state[2][2]],
-		[state[0][0], state[1][0], state[2][0]],
-		[state[0][1], state[1][1], state[2][1]],
-		[state[0][2], state[1][2], state[2][2]],
-		[state[0][0], state[1][1], state[2][2]],
-		[state[2][0], state[1][1], state[0][2]],
-	]
-	if [player, player, player] in winstate:
-		return True
-	else:
-		return False
+def game_over_all(state):
+    """
+    This function test if the human or computer wins
+    :param state: the state of the current board
+    :return: True if the human or computer wins
+    """
+    return game_over(state, HUMAN) or game_over(state, COMP)
 
 
 def empty_cells(state):
-	"""
-	Each cell empty will be added into cells' list
-	"""
-	cells = []
+    """
+    Each cell empty will be added into cells' list
+    :param state: the state of the current board
+    :return: a list of empty cells
+    """
+    cells = []
 
-	for x, row in enumerate(state):
-		for y, cell in enumerate(row):
-			if cell == 0:
-				cells.append([x, y])
-	return cells
+    for x, row in enumerate(state):
+        for y, cell in enumerate(row):
+            if cell == 0:
+                cells.append([x, y])
+    return cells
 
-# some bugs here
+
+def valid_move(x, y):
+    """
+    A move is valid if the cell chosen is empty
+    :param x: X coordinate
+    :param y: Y coordinate
+    :return: True if the board[x][y] is empty
+    """
+    if [x, y] in empty_cells(board):
+        return True
+    else:
+        return False
+
+
+def set_move(x, y):
+    """
+    Set the move on board, if the coordinates are valid
+    :param x: X coordinate
+    :param y: Y coordinate
+    """
+    if valid_move(x, y):
+        board[x][y] = HUMAN
+
 def minimax(state, depth, player):
+    """
+    AI function that choice the best move
+    :param state: current state of the board
+    :param depth: node index in the tree (0 <= depth <= 9)
+    :param player: an human or a computer
+    :return: a list with [the best row, best col, best score]
+    """
+    if player == COMP:
+        best = [-1, -1, -infinity]
+    else:
+        best = [-1, -1, +infinity]
 
-	if depth == 0 or gameover(state, player):
-		score = evaluate(state)
-		return [-1, -1, score]
+    if depth == 0 or game_over_all(state):
+        score = evaluate(state)
+        return [-1, -1, score]
 
-	if player == COMP:
-		best = [-1, -1, -infinity]
-	else:
-		best = [-1, -1, +infinity]
+    for cell in empty_cells(state):
+        x, y = cell[0], cell[1]
+        state[x][y] = player
+        score = minimax(state, depth - 1, -player)
+        state[x][y] = 0
+        score[0], score[1] = x, y
 
-	for cell in empty_cells(state):
-		x, y = cell[0], cell[1]
-		state[x][y] = player
-		score = minimax(state, depth - 1, -player)
-		state[x][y] = 0
-		score[0], score[1] = x, y
+        if player == COMP:
+            if score[2] > best[2]:
+                best = score  # max value
+        else:
+            if score[2] < best[2]:
+                best = score  # min value
 
-		if player == COMP:
-			if score[2] > best[2]:
-				best = score # max value
-		else:
-			if score[2] < best[2]:
-				best = score # min value
-
-	return best
-
-# DEBUG
-def render(state):
-	print('{:2} | {:2} | {:2}'.format(state[0][0], state[0][1], state[0][2]))
-	print('{:2} | {:2} | {:2}'.format(state[1][0], state[1][1], state[1][2]))
-	print('{:2} | {:2} | {:2}'.format(state[2][0], state[2][1], state[2][2]))
-
-while not gameover(board, COMP) and not gameover(board, HUMAN) and len(empty_cells(board)) > 0:
-	render(board)
-
-	print('Human')
-	x = int(input('x: ' ))
-	y = int(input('y: ' ))
-	board[x][y] = HUMAN
-
-	print('Computer')
-	ai = minimax(board, len(empty_cells(board)), COMP)
-	x = ai[0]
-	y = ai[1]
-	board[x][y] = COMP
-
-if gameover(board, COMP):
-	render(board)
-	print('Computer wins!')
-elif gameover(board, HUMAN):
-	print('Human wins!')
-else:
-	print('Draw')
+    return best
